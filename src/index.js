545d3761b1babda707cff5f6bd01a57b7dc91dc6
Yogeshwari-G <yogeshwarigopal63@gmail.com>
2025-03-23 19:08:27 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import "./style.css";
-import backgroundImage from "weatherbackground.jpg";
+import backgroundImage from ".assets/images/weatherbackground.jpg";
 
 const WeatherApp = () => {
   const [city, setCity] = useState("");
